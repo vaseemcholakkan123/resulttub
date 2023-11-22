58e900ee2ada617502d6dbace9295eb0c129a2b6
Vaseem Cholakkan <vaseemcholakkan@gmail.com>
2023-11-23 01:30:45 +0530

    404 added

--- resulttub/urls.py
+++ resulttub/urls.py
@@ -3,7 +3,16 @@ from django.urls import path , include , re_path
 from django.views.generic import TemplateView
 from .sitemap import ResultTubSitemap, StaticViewSitemap
 from django.contrib.sitemaps.views import sitemap
+from django.conf.urls import handler404
+from django.shortcuts import render
+from main.models import Category
+from django.db.models import Sum
 
+
+def error_404_view(request, exception):
+    return render(request, '404.html', status=404 , context={"categories" : Category.objects.annotate(total_views=Sum('blogs__views'))})
+
+handler404 = error_404_view
 
 sitemaps = {
     'sitemaps': ResultTubSitemap, 'flatpages': StaticViewSitemap,
